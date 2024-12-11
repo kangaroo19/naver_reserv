3184a8ddf719f8f006b62a0f9fef0c1afb4b0cd4
kangaroo19 <1000jjj@naver.com>
2024-12-11 22:58:04 +0900

    [feat] 다음버튼 클릭까지 구현
    - 자동로그인 구현

---
+++ index.js
@@ -0,0 +1,48 @@
+// https://blog.naver.com/tact_1663/223340585668 녹화본있음
+
+const { Builder, By, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+
+const options = new chrome.Options();
+options.addArguments("user-data-dir=C:\\user_data\\1000jjj"); // 브라우저를 최대화해서 실행
+options.addEx;
+// const BASIC_URL = "https://m.booking.naver.com/booking/13/bizes/987076/items/5336963"; // 실제로 사용할거
+const BASIC_URL = "https://m.booking.naver.com/booking/6/bizes/551459/items/4033828"; // 테스트용
+
+// START_DATE, START_DATE_TIEM 의 yyyy-mm-dd 값은 같아야함
+const START_DATE = "2024-12-17";
+const START_DATE_TIME = "2024-12-17T16%3A00%3A00%2B09%3A00";
+
+async function naverReserv() {
+  // WebDriver 초기화
+  const driver = await new Builder().forBrowser("chrome").setChromeOptions(options).build();
+  try {
+    // 페이지 열기
+    await driver.get(
+      `${BASIC_URL}?area=bmp&lang=ko&map-search=1&service-target=map-pc&startDate=${START_DATE}&startDateTime=${START_DATE_TIME}&theme=place`
+    );
+    await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+    await sleep(300);
+    // 로그인 화면 넘어가는 버튼
+    const nextButton = await driver.wait(
+      until.elementLocated(By.xpath('//*[@id="root"]/main/div[4]/div/button[2]')),
+      10000
+    );
+
+    // 클릭 가능한지 확인하고 클릭
+    await driver.wait(until.elementIsVisible(nextButton), 10000);
+    await nextButton.click();
+  } catch (err) {
+    console.error("에러 발생:", err);
+  } finally {
+    // 브라우저 닫기
+    // await driver.quit();
+  }
+}
+
+naverReserv();
+
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
